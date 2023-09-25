76e57a288e6baf7bff5cd63e26283e2770be591b
kittyplus <sogh_2002@hotmail.com>
2023-09-25 08:42:15 -0400

    a

---
+++ src/components/Salecard.js
@@ -0,0 +1,17 @@
+<div
+  class="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+  <div
+    class="border-b-2 border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-neutral-50">
+    70 kilos de nose
+  </div>
+  <div class="p-6">
+    <h5
+      class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+      Special title treatment
+    </h5>
+    <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+     contenido pedido.
+    </p>
+   
+  </div>
+</div>
