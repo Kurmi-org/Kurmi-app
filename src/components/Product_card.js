b5d4eed5659f8500411065d1a33e761ea747d07a
David203009 <vad0029612@est.univalle.edu>
2023-10-24 21:39:06 -0400

    fetch product home, Done

--- src/components/Product_card.js
+++ src/components/Product_card.js
@@ -3,7 +3,8 @@ import Link from "next/link";
 
 export default function ProductCard(props) {
   return (
-    <div className="w-5/6 md:max-w-sm mt-5 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+    <div className="w-5/6 mx-2 md:max-w-sm mt-5 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <Image
         className="rounded-t-lg"
         src="/next.config.jspublic/img/papas.jpg"
@@ -14,7 +15,7 @@ export default function ProductCard(props) {
 
       <div className="p-5">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Noteworthy technology acquisitions 2021
+          {props.name}
         </h5>
 
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
@@ -22,7 +23,7 @@ export default function ProductCard(props) {
           far, in reverse chronological order.
         </p>
         <Link
-          href="/productdetail"
+          href={`/productdetail/${props._id}`}
           className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Ver Mas
